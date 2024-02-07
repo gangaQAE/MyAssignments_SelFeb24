a6fb5144faa48fc36d50a5bdda7c5fd8e0b9aa45
gangaQAE <150062942+gangaQAE@users.noreply.github.com>
2024-02-07 10:50:26 +0530

    Add files via upload
    Day1_HomeAssignment

---
+++ Day1/HaDay1_DataTypes_Browser.java
@@ -0,0 +1,20 @@
+package homeAssignment.Day1;
+
+public class HaDay1_DataTypes_Browser {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 110.332f;
+		String browserName = "Firefox";
+		boolean pageLoaded = true;
+		int noOfBrowser = 8;
+		char browserLogo = 'J';
+		
+		System.out.println("Browser used : "+browserName);
+		System.out.println("Browser logo : "+browserLogo);
+		System.out.println("Confirm the webpage loaded status : "+pageLoaded);
+		System.out.println("No. of tabs opened : "+noOfBrowser);
+		System.out.println("Browser version : "+browserVersion);	
+	}
+
+}
---
+++ Day1/HaDay1_FindNumber.java
@@ -0,0 +1,17 @@
+package homeAssignment.Day1;
+
+public class HaDay1_FindNumber {
+
+	public static void main(String[] args) {
+
+		int number = 45;
+		
+		if(number >0) {
+			System.out.println(number +" is positive number");
+		}
+		else {
+			System.out.println(number +" is negative number");
+		}
+	}
+
+}
